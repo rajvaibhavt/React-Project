9667835b6f1abf86f36f252af58168fd8b660381
raj <your-rajvt172@gmail.com>
2026-03-31 22:15:14 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import axios from "./api/axiosconfig"
+function App() {
+  const getproduct = async()=>{
+try{
+ const res =  await axios.get('/product');
+ console.log(res.data);
+} catch(error){
+ console.log(error);
+}
+  };
+   useEffect(()=>{
+    getproduct()
+   },[]);
+  return (
+    <div> App </div>
+  )
+}
+
+export default App
