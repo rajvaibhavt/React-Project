59e363ad228997e0ed16fdca217a79e8f4e1f738
raj <rajvt172@gmail.com>
2026-04-01 23:58:30 +0530

    test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter }  from 'react-router-dom'
 
 import { store } from './store/Store.jsx'
 import { Provider } from 'react-redux'
