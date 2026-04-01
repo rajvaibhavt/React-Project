61be8bb739d6552ed6cf00f90c6ede6566202392
raj <your-rajvt172@gmail.com>
2026-04-01 23:06:46 +0530

    Fix Redux Provider setup and dependency issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import axios from "./api/axiosconfig"
 function App() {
-  const getproduct = async()=>{
+  const getproduct = async() => {
 try{
  const res =  await axios.get('/product');
  console.log(res.data);
 } catch(error){
- console.log(error);
-}
+ console.log(error); 
+}  
   };
    useEffect(()=>{
     getproduct()
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+import { store } from './store/Store.jsx'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <App />
  
+    <BrowserRouter>
+     <Provider  store = {store}>
+  <App />
+     </Provider>
+    
+    </BrowserRouter>
+
 )
